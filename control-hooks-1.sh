02dea86dcf506054fd79a747fa6077f1b8cf2653
Can Aloglu <aloglu.can@hotmail.com>
2023-03-01 17:00:44 +0300

    test

--- control-hooks-1.sh
+++ control-hooks-1.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 git status -s > .git/MODIFIED_FILES
 commit_type="dockerfile jenkinsfile makefile requirements yaml"
-echo ${commit_type}
 for types in $commit_type; do
-  echo ${types}
   if grep -q -i "$types" .git/MODIFIED_FILES; then
     if ! grep -e "ci" -e "build" .git/COMMIT_EDITMSG; then
       echo >&2 "Error: ci or build as a commit type"
