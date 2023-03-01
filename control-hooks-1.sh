6494ae98c3a567a339e312bfcd9453cf729def8e
Can Aloglu <aloglu.can@hotmail.com>
2023-03-01 17:09:17 +0300

    test

--- control-hooks-1.sh
+++ control-hooks-1.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
-#git status -s > .git/MODIFIED_FILES
+# git status -s > .git/MODIFIED_FILES
+# commit_type="dockerfile jenkinsfile makefile requirements yaml"
+# for types in $commit_type; do
+#   if grep -q -i "$types" .git/MODIFIED_FILES; then
+#     if ! grep -e "ci" -e "build" .git/COMMIT_EDITMSG; then
+#       echo >&2 "Error: ci or build as a commit type"
+#       rm .git/MODIFIED_FILES
+#       exit 1
+#     fi
+#   fi
+# done
+# rm .git/MODIFIED_FILES
+# exit 0
+
+
+
 commit_type="dockerfile jenkinsfile makefile requirements yaml"
 for types in $commit_type; do
-  if grep -q -i "$types" ${git status -s}; then
+  if ${git status -s} | grep -q -i "$types"; then
     if ! grep -e "ci" -e "build" .git/COMMIT_EDITMSG; then
       echo >&2 "Error: ci or build as a commit type"
-      r#m .git/MODIFIED_FILES
       exit 1
     fi
   fi
 done
-#rm .git/MODIFIED_FILES
 exit 0
